106a117ead1520744d150a43645f9f9ac792e5e7
097karimi <097karimi@gmail.com>
2021-10-23 15:14:21 +0330

    Add MEDIA_URL, MEDIA_ROOT and STATICFILES_DIR

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'aboutpage',
+    'contact',
+    'homepage',
+    'newsroom',
+    'order',
+    'orderitem',
+    'products',
+    'review',
+    'shippingaddress',
 ]
 
 MIDDLEWARE = [
@@ -125,10 +137,21 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, '/static/')
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
